9d475acd727607b3b92a9d59d581a80899ab695e
AlexK-2539 <alex.k2539@gmail.com>
2025-08-30 08:32:48 -0500

    finishing up homework by using cin option and learning how to call ASCII

--- C++ Programming/CH2Homework.cpp
+++ C++ Programming/CH2Homework.cpp
@@ -10,20 +10,23 @@ int main()
 {
     int num1; //user inputs number
     float num2; //user inputs number
-    char ranChar = 'T'; //random character
+    char ranChar; //random character indentifier
     
     //Getting num1 from user input
-    cout <<"Enter an number greater than 0: "<< endl;
+    cout <<"Please select a number greater than 0, then press enter: "<< endl;
     cin >> num1;
     cout <<"You have entered the number: "<< num1 << endl;
     
     //Getting num2 from user input
-    cout <<"Please enter a second number greater than 0, to include two numbers after the decimal (ex. 2.39)" << endl;
+    cout <<"Please select a second number greater than 0, to include two numbers after the decimal (ex. 2.39)" << endl;
+    cout <<"Continue by pressing the enter key" << endl;
     cin >> num2;
     cout <<"You have entered the number: " << num2 << endl;
     
     //Calling character variable to module
-    cout <<"The chosen character is: " << ranChar << endl;
+    cout <<"Type a random letter and press enter: " << endl;
+    cin >> ranChar;
+    cout <<"You have chosen -" << ranChar << "- as your character" << endl;
     int asciiValue = ranChar;
     cout <<"The ASCII value for the chosen character is: " << asciiValue << endl;
     
