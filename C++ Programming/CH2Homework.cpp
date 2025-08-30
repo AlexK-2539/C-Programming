7784d6771d494ca1edf53d17ec7bf386aed716f5
AlexK-2539 <alex.k2539@gmail.com>
2025-08-30 07:09:43 -0500

    update homework code

--- C++ Programming/CH2Homework.cpp
+++ C++ Programming/CH2Homework.cpp
@@ -1,11 +1,35 @@
-//  CH2Homework.cpp
-//  C++ Programming
-//  Created by Alex Keeling
+/* CH2Homework.cpp
+C++ Programming */
+
+// Created by Alex Keeling
 
 #include <iostream>
 using namespace std;
 
 int main()
 {
+    int num1; //user inputs number
+    float num2; //user inputs number
+    char ranChar = 'T'; //random character
+    bool is_hot_outside = true;
     
+    //Getting num1 from user input
+    cout <<"Enter an number greater than 0: "<< endl;
+    cin >> num1;
+    cout <<"You have entered the number: "<< num1 << endl;
+    
+    //Getting num2 from user input
+    cout <<"Please enter a second number greater than 0, to include decimal (ex. 2.39)" << endl;
+    cin >> num2;
+    cout <<"You have entered the number: " << num2 << endl;
+    
+    //Calling character variable to module
+    cout <<"The current variable for character is: " << ranChar << endl;
+    
+    //using the bool function to say its true that it is hot outside
+    cout <<"The weather during the day in Alabama is hot outside: " << is_hot_outside << endl;
+    
+    /
+    
+    return 0;
 }
