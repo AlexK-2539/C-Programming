e254b52dfc8df71a93f4f8b9b504fef44088de8e
AlexK-2539 <alex.k2539@gmail.com>
2025-08-30 08:12:24 -0500

    update

--- C++ Programming/CH2Homework.cpp
+++ C++ Programming/CH2Homework.cpp
@@ -18,19 +18,21 @@ int main()
     cout <<"You have entered the number: "<< num1 << endl;
     
     //Getting num2 from user input
-    cout <<"Please enter a second number greater than 0, to include decimal (ex. 2.39)" << endl;
+    cout <<"Please enter a second number greater than 0, to include two numbers after the decimal (ex. 2.39)" << endl;
     cin >> num2;
     cout <<"You have entered the number: " << num2 << endl;
     
     //Calling character variable to module
-    bool is_hot_outside = true;
+    
     cout <<"The current variable for character is: " << ranChar << endl;
     
     //using the bool function to say its true that it is hot outside
-    const int totalNum = num1 + num2;
+    bool is_hot_outside = true;
     cout <<"The weather during the day in Alabama is hot outside: " << is_hot_outside << endl;
     
     // Utilizing the const function
+    const float totalNum = num1 + num2;
+    cout <<"The total from the entered numbers equals: " << totalNum << endl;
     
     return 0;
 }
