2a555b6e05e1ac1d83858a4113df9f95ce274153
AlexK-2539 <alex.k2539@gmail.com>
2025-09-06 15:01:44 -0500

    hw3 starting

---
+++ C++ Programming/CH3Homework.cpp
@@ -0,0 +1,8 @@
+//
+//  CH3Homework.cpp
+//  C++ Programming
+//
+//  Created by Alex Keeling on 9/6/25.
+//
+
+#include "CH3Homework.hpp"
---
+++ C++ Programming/CH3Homework.hpp
@@ -0,0 +1,13 @@
+//
+//  CH3Homework.hpp
+//  C++ Programming
+//
+//  Created by Alex Keeling on 9/6/25.
+//
+
+#ifndef CH3Homework_hpp
+#define CH3Homework_hpp
+
+#include <stdio.h>
+
+#endif /* CH3Homework_hpp */
